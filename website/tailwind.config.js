528f92b3f8616044c2373c985339330390aaa2ac
Aaditya Paul <aadityapaul2006.programmer@gmail.com>
2024-11-17 01:23:34 +0530

    Add Ubuntu font support and refactor Home component layout

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        "ubuntu": "var(--font-ubuntu)",
+      },
     },
   },
   plugins: [],
